1079499111ac224cb80244cb957f64e02e6e5a96
Victor Tram <victortram1996@gmail.com>
2019-09-22 13:34:27 -0700

    API fetch

--- screens/AddCityScreen.js
+++ screens/AddCityScreen.js
@@ -1,24 +1,91 @@
 import React from 'react';
-import { StyleSheet, Text, View, Input} from 'react-native';
-import { Card, CardItem} from 'native-base'; 
+import { StyleSheet, Text, Keyboard, ScrollView, Alert, TouchableWithoutFeedback, AsyncStorage} from 'react-native';
+import { Form, Item, Input, Label, Button, Card, CardItem} from 'native-base'; 
 
-export default class InputScreen extends React.Component{
+export default class AddCityScreen extends React.Component{
 
     state = {
-        data: []
+      isLoading: true,
+      apiKey: '',  
+      cityName: "",
+      weather: [],
+      sys: [],
+      name: "",
     }
     
     static navigationOptions = {
         title: "Add City"
     }
 
+    saveCityData = async() => {
+      if(
+        this.state.city !== ""
+      ){
+        var cityData = {
+          cityName: this.state.cityName,
+        }
+      }else{
+        Alert.alert("Please enter in a city name.");
+      }
+      //await AsyncStorage.setItem( Date.now().toString(),JSON.stringify(cityData))
+      this.getWeatherData();
+      
+    }
+
+    componentDidMount(){
+      this.getWeatherData();
+    }
+
+    getWeatherData = () => {
+      return (
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}${this.state.apiKey}`)
+        .then( response => response.json())
+        .then( responseJson => {
+          // this.setState({
+          //   isLoading: false,
+          //   dataSource: this.state.dataSource.concat(responseJson.results)
+          // })
+          console.log(responseJson)
+        })
+        .catch(error => console.log(error))
+      )
+    }
     
 
     render(){
         return (
-            <View style={styles.container}>
-              <Text>Input Screen</Text>
-            </View>
+            <TouchableWithoutFeedback
+            onPress={ () => {
+              Keyboard.dismiss
+            }}
+            >
+              <ScrollView>
+              <Form>
+                <Item>
+                  <Label>
+                    City Name
+                  </Label>
+                  <Input
+                  autocorrect= {false}
+                  autoCapitalize= 'none'
+                  keyboardType="default"
+                  onChangeText={ (cityName) =>{
+                    this.setState({cityName})
+                  }}
+                  />
+                </Item>
+              </Form>
+              <Button
+              full
+              style={styles.button}
+              onPress = { () => {
+                this.saveCityData();
+              }}
+              >
+                <Text style={styles.buttonText}> Save</Text>
+              </Button>
+              </ScrollView>
+            </TouchableWithoutFeedback>
           );
     }
 }
@@ -30,5 +97,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button:{
+
+  },
+  buttonText:{
+
   },
 });
